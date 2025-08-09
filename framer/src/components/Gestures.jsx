ac706cb62d7d13e30007a259b0752469d808a788
AcePeQ <maciej.nojszewski22@gmail.com>
2025-08-09 12:13:06 +0200

    Gesture: 1st Ex:
    Zadanie 1: Interaktywny przycisk
    Stwórz przycisk z trzema stanami:
    
    Normalny: niebieski, scale 1
    Hover: jaśniejszy niebieski, scale 1.05
    Tap: ciemniejszy niebieski, scale 0.95
    Użyj variants!

--- framer/src/components/Gestures.jsx
+++ framer/src/components/Gestures.jsx
@@ -1,7 +1,35 @@
 import styles from "./Gestures.module.css";
 
+import { motion } from "motion/react";
+
+const buttonVariants = {
+  idle: {
+    backgroundColor: "#0026ff",
+    scale: 1,
+  },
+  hover: {
+    backgroundColor: "#1f87ff",
+    scale: 1.05,
+  },
+  tap: {
+    backgroundColor: "#0051ff",
+    scale: 0.95,
+  },
+};
+
 function Gestures() {
-  return <div className={styles.wrapper}></div>;
+  return (
+    <div className={styles.wrapper}>
+      <motion.button
+        variants={buttonVariants}
+        initial="idle"
+        whileHover="hover"
+        whileTap="tap"
+      >
+        Button
+      </motion.button>
+    </div>
+  );
 }
 
 export default Gestures;
