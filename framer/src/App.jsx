32f07e971acfe92d2d101a74070dfab1e1b88de7
AcePeQ <maciej.nojszewski22@gmail.com>
2025-08-09 12:05:32 +0200

    display gesture component

--- framer/src/App.jsx
+++ framer/src/App.jsx
@@ -1,6 +1,7 @@
 import Counter from "./components/Counter";
 import Exer1 from "./components/Exer1";
 import Exer2 from "./components/Exer2";
+import Gestures from "./components/Gestures";
 import Navbar from "./components/Navbar";
 import Navbar2 from "./components/Navbar2";
 import SimpleAnimation from "./components/SimpleAnimation";
@@ -15,7 +16,8 @@ function App() {
       {/* <Exer2 /> */}
 
       {/* <Navbar /> */}
-      <Navbar2 />
+      {/* <Navbar2 /> */}
+      <Gestures />
     </>
   );
 }
