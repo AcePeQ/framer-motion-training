21bd761e12d3a9c414fe827a3b8ffd14eff80c73
AcePeQ <maciej.nojszewski22@gmail.com>
2025-08-11 18:12:03 +0200

    progress

--- framer/src/components/ProgressBar/ProgressBar.jsx
+++ framer/src/components/ProgressBar/ProgressBar.jsx
@@ -1,8 +1,16 @@
-import { useMotionValue, useTransform, motion } from "motion/react";
+import {
+  useMotionValue,
+  useTransform,
+  motion,
+  useSpring,
+  useMotionValueEvent,
+} from "motion/react";
 import styles from "./ProgressBar.module.css";
+import { useState } from "react";
 
 function ProgressBar() {
   const progress = useMotionValue(0);
+  const [percent, setPercent] = useState(0);
 
   const width = useTransform(progress, [0, 100], ["0%", "100%"]);
 
@@ -12,8 +20,14 @@ function ProgressBar() {
     ["#ff0000", "#ffe600", "#1eff00"]
   );
 
+  useMotionValueEvent(progress, "change", (latest) => {
+    setPercent(latest);
+  });
+
   return (
     <>
+      <motion.div>Procenty: {percent}%</motion.div>
+
       <input
         className={styles.range}
         type="range"
